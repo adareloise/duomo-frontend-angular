2991c01212a840939cc2279386d92c6a78350def
adareloise <dutreras369@gmail.com>
2022-03-25 16:57:21 -0300

    up angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { RegionesComponent } from './regiones/regiones.component';
+import { RegionService } from './regiones/region.service';
+import { ListarComponent } from './listar/listar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http'
+
+const routes: Routes = [
+  {path:'', redirectTo: '/formulario', pathMatch: 'full'},
+  {path:'formulario', component: FormularioComponent},
+  {path:'listar', component: ListarComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HeaderComponent,
+    FooterComponent,
+    FormularioComponent,
+    RegionesComponent,
+    ListarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    RegionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
